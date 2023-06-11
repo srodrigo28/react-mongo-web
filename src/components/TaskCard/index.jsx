aa41b35cf61e95be1f41e3b5bb52af5db8fe8226
Sebastiao <rodrigoexer1@gmail.com>
2023-06-11 17:10:02 -0300

    home-listed-witch-bug

--- src/components/TaskCard/index.jsx
+++ src/components/TaskCard/index.jsx
@@ -1,18 +1,24 @@
+/* eslint-disable react/prop-types */
 import * as S from "./styles";
-import iconDefault from '../../assets/default.png'
+import { format } from 'date-fns'
+import { useMemo } from "react";
+import typeIcons from "./../../services/typeIcons"
 
-export function TaskCard() {
+export function TaskCard({type, title, when}) {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const date = useMemo(() => format(new Date(when), 'dd/MM/yyyy'));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const hour = useMemo(() => format(new Date(when), 'HH:mm'));
     return (
         <S.Container>
-            
             <S.TopCard>
-                <img src={iconDefault} alt="" />
-                <h3>Título da Tarefa</h3>
+                <img src={typeIcons[type]} alt="icone type" />
+                <h3>{title}</h3>
             </S.TopCard>
 
             <S.BottomCard>
-                <strong>17/10/2020</strong>
-                <span>10:00</span>
+                <strong>{date}</strong>
+                <span>{hour}</span>
             </S.BottomCard>
 
         </S.Container>
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,43 +1,61 @@
-import { Header } from './../../components/Header'
-import { Footer } from './../../components/Footer'
 import { FilterCard } from './../../components/FilterCard'
 import { TaskCard } from './../../components/TaskCard'
+import { Header } from './../../components/Header'
+import { Footer } from './../../components/Footer'
 import { useEffect, useState } from "react";
-import * as S from './styles'
+import {v4 as uuidv4} from 'uuid';
 import api from './../../services/api'
+import * as S from './styles'
 
 export function Home() {
+    const id = uuidv4();
     const [filterActived, setFilterActived] = useState("all");
     const [tasks, setTasks] = useState([]);
-    const makadress = "123123123";
+
     async function loadTasks() {
-        await api.get(`/task/filter/${filterActived}/${makadress}`)
+        await api.get(`/task/filter/${filterActived}/00:1B:44:11:3A:B9`)
         .then(response => {
             setTasks(response.data);
         })
     }
     useEffect(() => {
         loadTasks();
-    }, [filterActived]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
+
     return (
         <S.Container>
-            <p>{!tasks}</p>
-            <Header onClick={loadTasks} />
+            <Header/>
             <S.FilterArea>
-                <button onClick={ () => setFilterActived("all") } >
-                    <FilterCard title="Todos" actived={filterActived == ("all") }/>
+                <button  onClick={ () => setFilterActived("all") } >
+                    <FilterCard 
+                        title="Todos"   
+                        actived={( filterActived === ("all") | null ) }
+                    />
                 </button>
-                <button onClick={ () => setFilterActived("today")} >
-                    <FilterCard title="Hoje" actived={filterActived == ("today") }/>
+                <button  onClick={ () => setFilterActived("today") } >
+                    <FilterCard 
+                        title="Hoje"
+                        actived={(filterActived === ("today") | null )}
+                    />
                 </button>
-                <button onClick={ () => setFilterActived("week")} >
-                    <FilterCard title="Semana" actived={filterActived == ("week")} />
+                <button onClick={ () => setFilterActived("week") } >
+                    <FilterCard 
+                        title="Semana"
+                        actived={(filterActived === ("week") | null )}
+                    />
                 </button>
-                <button onClick={ () => setFilterActived("month")} >
-                    <FilterCard title="Mês" actived={filterActived == ("month")} />
+                <button onClick={ () => setFilterActived("month") } >
+                    <FilterCard 
+                        title="Mês"
+                        actived={(filterActived === ("month") | null )}
+                    />
                 </button>
-                <button onClick={ () => setFilterActived("year")} >
-                    <FilterCard title="Ano" actived={filterActived == ("year")} />
+                <button onClick={ () => setFilterActived("year") } >
+                    <FilterCard 
+                        title="Ano"
+                        actived={(filterActived === ("year") | null )}
+                    />
                 </button>
             </S.FilterArea>
 
@@ -46,9 +64,15 @@ export function Home() {
             </S.Title>
 
             <S.Content>
-                <TaskCard />
+                { tasks.map(t=> (
+                    <TaskCard
+                        key={t._id+id}
+                        type={t.type}
+                        title={t.title} 
+                        when={t.when}
+                    />
+                ))}
             </S.Content>
-
             <Footer />
         </S.Container>
     )
