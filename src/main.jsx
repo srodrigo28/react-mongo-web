c131afce185ceb0e2fb4de96691ba3ab298fad85
Sebastiao <rodrigoexer1@gmail.com>
2023-06-13 03:24:38 -0300

    updated-project-past

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { Home } from './views/Home'
-// import { Task } from './views/Task'
-// import { User } from './views/User'
-import { Contas } from './views/Contas'
+import { Task } from './views/Task'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Contas />
+      <Task/>
   </React.StrictMode>,
 )
--- src/views/QrCode/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export function QrCode() {
-    return (
-        <Container>
-            <h1>QrCode</h1>
-        </Container>
-    )
-}
